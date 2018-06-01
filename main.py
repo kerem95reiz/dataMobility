ab398684012153434ab266eebc766ffc38926bf9
Kerem <0256199+kerem95reiz@users.noreply.github.com>
2018-06-01 20:38:28 +0200

    intial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Coordinates needed for the basemap start position
+# Istanbul
+# westlimit=28.5804; southlimit=40.879; eastlimit=29.4537; northlimit=41.1743
+
+if __name__ == '__main__':
+    print("main")
