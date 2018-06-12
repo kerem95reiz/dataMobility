420f9a36f376c171b75e7af2b410b0e4bebaa848
Kerem <0256199+kerem95reiz@users.noreply.github.com>
2018-06-12 14:00:02 +0200

    first structure created

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-# Coordinates needed for the basemap start position
-# Istanbul
-# westlimit=28.5804; southlimit=40.879; 
-# eastlimit=29.4537; northlimit=41.1743
+from folium import Map
 
-from mpl_toolkits.basemap import Basemap
-import matplotlib.pyplot as plt
-# import matplotlib.cm
+def read_csv_data():
+    return 0
 
-# from matplotlib.patches import Polygon
-# from matplotlib.collections import PatchCollection
-# from matplotlib.colors import Normalize
+def create_map(start_coords: tuple):
+    return 0
+
+def put_marker(coord: tuple, location: Map):
+    return 0
 
 if __name__ == '__main__':
-    fig, ax = plt.subplots(figsize=(10, 20))
-    m = Basemap(
-        resolution='c',
-        projection='merc',
-        lat_0=40.9, lon_0=28.9,
-        llcrnrlon=28.5, llcrnrlat=40.8, urcrnrlon=29.4, urcrnrlat=41.1
-    )
-
-    m.drawmapboundary(fill_color='#46bcec')
-    m.fillcontinents(color='#f2f2f2', lake_color='#46bcec')
-    m.drawmapboundary()
-    # m.fillcontinents()
-    # m.drawcoastlines()
+    print("Nothing been done yet!")
