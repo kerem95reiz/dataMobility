bc535f112776a7b7bae4b4921d149a3accbcdd1d
Kerem <0256199+kerem95reiz@users.noreply.github.com>
2018-06-12 14:50:39 +0200

    data is put on a map and saved as a html

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-from folium import Map
+from folium import Map, Marker
+from csv import reader
+from pathlib import Path
+from time import sleep
 
-def read_csv_data():
-    return 0
+def read_csv_data(path):
+    data = []
+    with open(path, 'r', newline='') as file:
+        csv_reader = reader(file)
+        for line in csv_reader:
+            if line[2] != 'time':
+                data.append([float(line[0]), float(line[1]), line[2]])
+
+    return data
 
 def create_map(start_coords: tuple):
-    return 0
 
-def put_marker(coord: tuple, location: Map):
-    return 0
+    location = Map(start_coords[:2])
+    location.zoom_start = 15
+    return location
+
+def put_marker(coord: list, location: Map):
+    marker = Marker(coord[:2])
+    marker.add_to(location)
+
 
 if __name__ == '__main__':
-    print("Nothing been done yet!")
+    data_path = "./data/trajectories_1.csv"
+    data = read_csv_data(data_path)
+
+    area = create_map(data[1])
+
+    for data_point in data:
+        put_marker(data_point, area)
+    area.save(str(Path.home()) + "/Desktop/map.html")
+
