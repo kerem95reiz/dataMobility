647a54b9aa396fd61954e579c2034bb4c8422085
Kerem <0256199+kerem95reiz@users.noreply.github.com>
2018-06-15 11:34:40 +0200

    different trs with different colors

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PIL import Image
 from base64 import b64encode
 from os import listdir
 from os.path import isfile, join
+from random import choice
 
 def read_csv_data(path):
     data = []
@@ -24,32 +25,32 @@ def create_map(start_coords: tuple):
     location.zoom_start = 12
     return location
 
-def put_marker(coord: list, location: Map):
+def put_marker(coord: list, location: Map, color_icon: str):
+    
     # create a marker with a specific coords
-    marker = Marker(coord[:2])
+    marker = Marker(coord[:2], icon=Icon(color=color_icon))
     # add the marker to the passed the map
     marker.add_to(location)
-
-
 
 if __name__ == '__main__':
     trajectory_path = './data/'
     trajectory_datas = []
     number_of_files_tbr = 3 # tbr = to be read
+    colors_for_icons = ['red', 'blue', 'green', 'purple', 'orange', 'darkred', 'beige', 'darkblue', 'gray']
+
     # Get all the files with extension '.csv' and put them into an array with the folder name
     data_paths = [join(trajectory_path, path) for path in listdir(trajectory_path) if isfile(join(trajectory_path, path)) and ('.csv' in path) ]
+
+    # Extract the data from the trajectory files with the given number of files
     for x in range(number_of_files_tbr):
         trajectory_datas.append(read_csv_data(data_paths[x]))
 
-    # data = read_csv_data(data_path)
-
+    # Create the map with a sample coord
     area = create_map(trajectory_datas[1][1])
 
     for tr in trajectory_datas:
+        color_of_icon = choice(colors_for_icons)
         for data_point in tr:
-            put_marker(data_point, area)
+            put_marker(data_point, area, color_of_icon)
 
     area.save(str(Path.home()) + "/Desktop/map.html")
-
-    # ima = Image.open('./icons/lnr-map-marker.png')
-    # print(ima.tobytes())
