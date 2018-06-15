f0b1e941a12f7bc51324b658aed0c48b66d9b25f
Kerem <0256199+kerem95reiz@users.noreply.github.com>
2018-06-15 11:14:41 +0200

    more than one file can be read and adjusted to the new data set format

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from folium import Map, Marker
+from folium import Map, Marker, Icon, CustomIcon
 from csv import reader
 from pathlib import Path
 from time import sleep
+from PIL import Image
+from base64 import b64encode
+from os import listdir
+from os.path import isfile, join
 
 def read_csv_data(path):
     data = []
     with open(path, 'r', newline='') as file:
         csv_reader = reader(file)
         for line in csv_reader:
-            if line[2] != 'time':
-                data.append([float(line[0]), float(line[1]), line[2]])
-
+            if 'lat' not in line:
+                data.append([float(line[0]), float(line[1])])
     return data
 
 def create_map(start_coords: tuple):
-
     location = Map(start_coords[:2])
-    location.zoom_start = 15
+    location.zoom_start = 12
     return location
 
 def put_marker(coord: list, location: Map):
@@ -24,13 +26,25 @@ def put_marker(coord: list, location: Map):
     marker.add_to(location)
 
 
+
 if __name__ == '__main__':
-    data_path = "./data/trajectories_1.csv"
-    data = read_csv_data(data_path)
+    trajectory_path = './data/'
+    trajectory_datas = []
+    number_of_files_tbr = 3 # tbr = to be read
+    # Get all the files with extension '.csv' and put them into an array with the folder name
+    data_paths = [join(trajectory_path, path) for path in listdir(trajectory_path) if isfile(join(trajectory_path, path)) and ('.csv' in path) ]
+    for x in range(number_of_files_tbr):
+        trajectory_datas.append(read_csv_data(data_paths[x]))
 
-    area = create_map(data[1])
+    # data = read_csv_data(data_path)
 
-    for data_point in data:
-        put_marker(data_point, area)
+    area = create_map(trajectory_datas[1][1])
+
+    for tr in trajectory_datas:
+        for data_point in tr:
+            put_marker(data_point, area)
+
     area.save(str(Path.home()) + "/Desktop/map.html")
 
+    # ima = Image.open('./icons/lnr-map-marker.png')
+    # print(ima.tobytes())
